b37aa63991dcbc8b265da37f83632aa1324ff0a3
dude <dude1410@mail.ru>
2022-09-22 00:10:06 +0300

    initial

---
+++ src/main/java/kafka/MyKafkaProducer.java
@@ -0,0 +1,34 @@
+package kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class MyKafkaProducer {
+    
+    public static void main(String[] args) {
+
+        String bootStrapServer = "127.0.0.1:9092";
+
+        // create Producer properties
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootStrapServer);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<String, String>(properties);
+
+        // create producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world" +
+                "to kafka from java app");
+
+        // send data
+        producer.send(record);
+
+
+    }
+}
