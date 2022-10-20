c1f5b6655d3b1479ee4e76ade1d2e24999172599
dude <dude1410@mail.ru>
2022-10-20 23:54:20 +0300

    + elastic

---
+++ kafka-consumer-elasticsearch/src/main/java/elastic/ElasticSearchConsumer.java
@@ -0,0 +1,66 @@
+package elastic;
+
+import org.apache.http.HttpHost;
+import org.apache.http.auth.AuthScope;
+import org.apache.http.auth.UsernamePasswordCredentials;
+import org.apache.http.client.CredentialsProvider;
+import org.apache.http.impl.client.BasicCredentialsProvider;
+import org.apache.http.impl.nio.client.HttpAsyncClientBuilder;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestClientBuilder;
+import org.elasticsearch.client.RestHighLevelClient;
+
+import java.io.InputStream;
+import java.util.Properties;
+
+public class ElasticSearchConsumer {
+
+    private static String hostname = "";
+    private static String username = "";
+    private static String password = "";
+
+    public static RestHighLevelClient createClient() {
+
+        final CredentialsProvider credentialsProvider = new BasicCredentialsProvider();
+        credentialsProvider.setCredentials(AuthScope.ANY, new UsernamePasswordCredentials(username, password));
+
+        RestClientBuilder builder = RestClient.builder(
+                new HttpHost(hostname, 443, "https")).setHttpClientConfigCallback(
+                new RestClientBuilder.HttpClientConfigCallback() {
+                    @Override
+                    public HttpAsyncClientBuilder customizeHttpClient(HttpAsyncClientBuilder httpAsyncClientBuilder) {
+                        return httpAsyncClientBuilder.setDefaultCredentialsProvider(credentialsProvider);
+                    }
+                });
+
+        RestHighLevelClient client = new RestHighLevelClient(builder);
+        return client;
+    }
+
+    public static void initialize() {
+        try (InputStream input = ElasticSearchConsumer.class.getClassLoader().getResourceAsStream("config.properties")) {
+
+            Properties prop = new Properties();
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+            //load a properties file from class path, inside static method
+            prop.load(input);
+            //get the property values
+            hostname = prop.getProperty("elastic.hostname");
+            username = prop.getProperty("elastic.username");
+            password = prop.getProperty("elastic.password");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        initialize();
+        System.out.println(hostname);
+        System.out.println(username);
+        System.out.println(password);
+    }
+}
